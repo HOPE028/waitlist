195c19c04f1719df62c65829c02c369a0eea2a55
Pasha Khoshkebari <pasha.khoshkeba@gmail.com>
2022-06-26 12:16:12 -0500

    upload

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route
-            path='/'
+            path='/waitlist/*'
             element={currentUser !== null ? <DashBoard /> : <Login />}
           />
           <Route
