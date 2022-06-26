935c95e3e8e863007db952155641aa0686e7673f
Pasha Khoshkebari <pasha.khoshkeba@gmail.com>
2022-06-25 21:45:39 -0500

    security

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -8,13 +8,13 @@ import { getAuth } from 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyAY5Q0wuMneH3qa5XUHo5PYLKe_1d0Nu4I',
-  authDomain: 'daycare-a6fe2.firebaseapp.com',
-  projectId: 'daycare-a6fe2',
-  storageBucket: 'daycare-a6fe2.appspot.com',
-  messagingSenderId: '476464804852',
-  appId: '1:476464804852:web:b79b17b327cc37cde23fa1',
-  measurementId: 'G-QZGY3GW58W',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
